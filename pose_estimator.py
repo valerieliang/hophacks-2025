70f89b5a17a09c27808d02ddf7026402543c45c6
Valerie Liang <143350210+valerieliang@users.noreply.github.com>
2025-09-13 19:27:43 -0400

    added keypoints legend

--- pose_estimator.py
+++ pose_estimator.py
@@ -11,3 +11,25 @@ class PoseEstimator:
         # list of [x, y, conf] per joint
         keypoints = results[0].keypoints
         return annotated_frame, keypoints
+
+"""
+List of keypoints:
+0: nose
+1: left_eye
+2: right_eye
+3: left_ear
+4: right_ear
+5: left_shoulder
+6: right_shoulder
+7: left_elbow
+8: right_elbow
+9: left_wrist
+10: right_wrist
+11: left_hip
+12: right_hip
+13: left_knee
+14: right_knee
+15: left_ankle
+16: right_ankle
+Keypoint format: [x, y, confidence]
+"""
